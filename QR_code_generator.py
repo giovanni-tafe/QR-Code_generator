9da7b062ba37703fde2dc1a250d1099ba205f654
Giovanni TAFE <20114918@tafe.wa.edu.au>
2024-05-01 16:52:27 +0800

    feat: add assets

--- QR_code_generator.py
+++ QR_code_generator.py
@@ -1,20 +1,19 @@
 import qrcode
 
-# Enter url of any website here.
-input_URL = "https://www.google.com/"
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=15,
-    border=4,
-)
+def generateQRCode(input_URL):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=15,
+        border=4,
+    )
 
-qr.add_data(input_URL)
-qr.make(fit=True)
+    qr.add_data(input_URL)
+    qr.make(fit=True)
 
-# convert into image
-img = qr.make_image(fill_color="red", back_color="white")
-img.save("url_qrcode.png")
+    # convert into image
+    img = qr.make_image(fill_color="red", back_color="white")
+    img.save("static/assets/img/url_qrcode.png")
 
-print(qr.data_list)
+    print(qr.data_list)
